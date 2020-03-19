0253dfb97a533a239977cdf219b9e36efd505509
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-03-19 16:43:25 -0300

    Link CSS and JS, add memoryGame(), shuffleCards() and checkifPair() methods

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,30 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    let j, aux = 0 
+    for(let i = this.cards.length - 1; i>= 1; i--){
+      j = Math.round(Math.random() * i)
+      aux = this.cards[i]
+      this.cards[i] = this.cards[j]
+      this.cards[j] = aux
+    }
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++
+    if(card1 === card2) {
+      this.pairsGuessed += 1
+      return true
+    }
+    return false
+  }
+
+  isFinished() {
+    
+  }
 }
